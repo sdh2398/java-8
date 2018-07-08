1803d0e6e8d02d670dace1097999b71ee551f72b
sdh2398 <ehdgus2398@naver.com>
2018-07-09 01:14:55 +0900

    order, reduce test

--- src/test/JavaStreamTest.java
+++ src/test/JavaStreamTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.BeforeClass;
@@ -96,6 +98,7 @@ public class JavaStreamTest {
 
 	@Test
 	public void test_findFirst() throws Exception {
+		// findFirst는 병렬적으로 실행되도 첫 번째 원소를 선택할 수 있다.
 		assertEquals("a", Stream.of("a", "b", "c").parallel().findFirst().get());
 	}
 
@@ -123,7 +126,53 @@ public class JavaStreamTest {
 		// assertEquals(3, counter);
 		assertEquals(Arrays.asList("ABC5", "ABCDEF5"), stream3.collect(toList()));
 		assertEquals(5, counter);
+
+		counter = 0;
+		long size = list2.stream().map(element -> {
+			wasCalled();
+			return element.substring(0, 3);
+		}).skip(2).count();
+		System.out.println("size = " + size);
+	}
+
+	@Test
+	public void test_orderOfExecution() throws Exception {
+		// skip()의 위치에 따라 연산의 수가 줄어든다.
+		counter = 0;
+		long size = list2.stream().map(element -> {
+			wasCalled();
+			return element.substring(0, 3);
+		}).skip(2).count();
+
+		assertEquals(5, counter);
+		assertEquals(3, size);
+
+		counter = 0;
+		size = list2.stream().skip(2).map(element -> {
+			wasCalled();
+			return element.substring(0, 3);
+		}).count();
+
+		assertEquals(3, counter);
+		assertEquals(3, size);
+	}
+
+	@Test
+	public void test_reduce() throws Exception {
+		OptionalInt reduced = IntStream.range(1, 4).reduce((a, b) -> a + b);
+		assertEquals(6, reduced.getAsInt());
+
+		int reducedTwoParams = IntStream.range(1, 4).reduce(10, (a, b) -> a + b);
+		assertEquals(16, reducedTwoParams);
+
+		int reducedParallel = Arrays.asList(1, 2, 3).parallelStream().reduce(10, (a, b) -> a + b, (a, b) -> {
+			System.out.println("combiner was called");
+			return a + b;
+		});
+		// 모든 요소를 병렬적으로 실행하여 11 + 12 + 13이 된다.
+		assertEquals(36, reducedParallel);
 	}
 	
 	
+
 }
